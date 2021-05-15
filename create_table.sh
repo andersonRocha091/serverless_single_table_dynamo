aa6a1f87c5b411860ccbe2843b99fc6030d119d7
Anderson <andersonecomp091@gmail.com>
2021-05-15 18:37:06 -0300

    creating global index for booking by location

--- create_table.sh
+++ create_table.sh
@@ -4,9 +4,22 @@ aws \
   --attribute-definitions \
    AttributeName=propertyId,AttributeType=S \
    AttributeName=sortKey,AttributeType=S \
+   AttributeName=country_city,AttributeType=S \
   --key-schema \
    AttributeName=propertyId,KeyType=HASH \
    AttributeName=sortKey,KeyType=RANGE \
   --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1 \
   --stream-specification StreamEnabled=TRUE,StreamViewType=NEW_AND_OLD_IMAGES \
   --billing-mode PAY_PER_REQUEST \
+  --global-secondary-indexes \
+    "[
+        {
+            \"IndexName\": \"Properties_GSI1\",
+            \"KeySchema\": [{\"AttributeName\":\"country_city\",\"KeyType\":\"HASH\"},
+                            {\"AttributeName\":\"propertyId\",\"KeyType\":\"RANGE\"}],
+            \"Projection\":{
+                \"ProjectionType\":\"INCLUDE\",
+                \"NonKeyAttributes\":[\"ownerId\",\" title\", \"description\", \"city\", \"country\"]
+            }
+        }
+    ]"
